90ae04bc9d8def7f425b7f6f4038f07e23c922cb
Martins Ngene <martinsngene9@gmail.com>
2023-02-06 21:28:48 +0100

    update: Fix Footer responsiveness

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -6,12 +6,12 @@ import { footerIcons } from "../constants/Footer.Schema";
 
 const Footer = () => {
   return (
-    <div className='w-full px-4 lg:px-[3rem] h-[7.5vh] flex items-center justify-between bg-white'>
-      <div className='text-[0.75rem] lg:text-[1rem]  text-[#000] font-[700] flex items-center justify-center'>
+    <div className='w-full py-2 lg:py-0 px-4 lg:px-[3rem] h-[8vh]  lg:h-[7.5vh] flex flex-col lg:flex-row items-center justify-between bg-white'>
+      <div className='hidden xl:flex text-[0.75rem] lg:text-[1rem]  text-[#000] font-[700] items-center justify-center'>
         <span>&copy;</span>
         <span className='ml-2'>2023. Martins Ngene</span>
       </div>
-      <div className='flex items-center justify-between text-[1rem] md:text-[2rem] md:w-[500px]'>
+      <div className='mb-1 lg:mb-0 flex items-center justify-between text-base md:text-2xl lg:text-[2rem] w-4/5 md:w-[500px]'>
         {footerIcons.map((icon, index) => {
           return (
             <div className='cursor-pointer' key={index}>
